b6f2f92dc83335e5f0d602e56ab2d3189a614eb3
Amaydeveloper <amaydeveloper@gmail.com>
2022-07-29 21:26:12 +0200

    header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
-      <h1> welcome to my online store brother</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1> welcome to my online store brother</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 };
